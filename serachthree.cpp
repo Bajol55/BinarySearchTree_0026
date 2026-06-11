af062cfe8562f3e9fcb5bca85db7ac25533a83ba
Bajol55 <devarafadil35@gmail.com>
2026-06-11 16:20:04 +0700

    menambahkan char switch

--- serachthree.cpp
+++ serachthree.cpp
@@ -133,4 +133,19 @@ int main()
 
     while (true)
     {
+        cout <<"\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5)\n";
+    
+        char ch;
+        cin >> ch;
+
+        cout << endl;
         
+        switch (ch)
+        {
+       
